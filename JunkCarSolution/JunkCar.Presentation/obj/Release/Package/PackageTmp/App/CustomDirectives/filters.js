827c2fa5ddba10757b88cb81bf2a9e334673e02c
Hassan Mustafa Baig <mirzahassanbaig2006@gmail.com>
2015-02-19 16:09:07 +0500

    Presentation Fix

--- JunkCarSolution/JunkCar.Presentation/obj/Release/Package/PackageTmp/App/CustomDirectives/filters.js
+++
@@ -1,9 +0,0 @@
-﻿angular.module('filters', []).filter('textOrNumber', function ($filter) {
-    return function (input, fractionSize) {
-        if (isNaN(input)) {
-            return input;
-        } else {
-            return $filter('number')(input, fractionSize);
-        };
-    };
-});
--- JunkCarSolution/JunkCar.WebAPI/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JunkCar.WebAPI.WebApiApplication" Language="C#" %>
